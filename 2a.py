2addd6db6add81de40aa48753d6c1961b2634d50
Janepoor <fjmjp@126.com>
2017-04-10 14:44:30 -0400

    Write 2a

--- 2a.py
+++ 2a.py
@@ -1,8 +1,38 @@
-"""
-
-Create ICMP(ping)
-
-
-
+# COMS 4701 Jianpu Ma
 
 """
+
+Create ICMP(ping) between 2 machine
+show the response
+Monitor the packet : tcpdump -i eth0 -vv -XX
+Copy the ouput from tcpdump and response to result file
+
+"""
+
+import sys
+from scapy.all import sr1,IP,ICMP
+
+
+def main():
+
+    try:
+        ip = sys.argv[1]
+    except IndexError:
+        print "Error: Provide IP address"
+        exit(0)
+
+
+    try:
+        p = sr1(IP(dst=ip) / ICMP() / Raw(load="TEST"))
+    except socket.error:
+        print "Can't open socket maybe you are not root?"
+        exit(0)
+    if p:
+        """
+        If got response print it.
+        """
+        p.show()
+
+
+if __name__ == "__main__":
+    main()
