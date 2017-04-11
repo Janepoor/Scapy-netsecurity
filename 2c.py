c859fb5e5c54779c0d23d93e6d67ebd01c50f853
Janepoor <fjmjp@126.com>
2017-04-10 21:08:41 -0400

    Add 2c

---
+++ 2c.py
@@ -0,0 +1,45 @@
+import sys
+import logging
+import random
+import string
+
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+from scapy.all import *
+
+class Packet(object):
+    def __init__(self, sport, dport):
+
+        self.sport = sport
+        self.dport = dport
+        self.loopback = "127.0.0.1"
+        self.sendTCP()
+
+    def sendTCP(self):
+        """ Part (a) sending TCP packets
+        """
+        for i in range(3000, 3021):
+            send(IP(dst=self.loopback) / TCP(dport=i))
+        self.sendRandom()
+
+    def sendRandom(self):
+        """Part(b)  sending random TCP packets
+        """
+        for i in range(0, 5):
+            """
+            Generate 5 packets and then send them
+            """
+            randomstr = ''.join(random.choice(string.letters) for x in range(10))
+            send(IP(dst=self.loopback) / TCP(sport=self.sport, dport=self.dport) / Raw(load=randomstr))
+
+def main():
+    try:
+        sport = int(sys.argv[1])
+        dport = int(sys.argv[2])
+    except IndexError:
+        print "Error: Run as python <2c.py> <source_port> <dst_port> "
+        exit(0)
+    p = Packet(sport, dport)
+
+
+if __name__ == "__main__":
+    main()
---
+++ loopback.py
@@ -0,0 +1,33 @@
+
+import string
+from scapy.all import *
+import sys
+
+def loop():
+    try:
+        sourceport = int(sys.argv[1])
+        destport = int(sys.argv[2])
+    except IndexError as E:
+        print "E"
+
+
+    loopbackip="127.0.0.1"ip
+    for i in range(3000, 3021):
+        send(IP(dst=loopback) / TCP(dport=i))
+
+    """
+    Generate 5 packets and then send them
+    """
+
+    for i in range(0, 5):
+
+        randomstr = ''.join(random.choice(string.letters) for x in range(10))
+        send(IP(dst=loopbackip) / TCP(sport=sourceport, dport=destport) / Raw(load=randomstr))
+
+    p = Packet(sourceport, destport)
+
+def main():
+    loop()
+
+if __name__ == '__main__':
+    main()
